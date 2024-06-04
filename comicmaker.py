d19dc65c5ae3d321793692952430c5671a0127c7
tan.tu <tan.tungo@gmail.com>
2024-06-05 03:27:35 +0700

    refactor

--- comicmaker.py
+++ comicmaker.py
@@ -1,53 +1,48 @@
-import sys
-import os
+"""Generate html from image folder."""
 import argparse
-import string
+import webbrowser
+from pathlib import Path
 
-imgExt = ['.jpg', '.jpeg', '.png', '.bmp', '.webp']
+IMAGE_EXTENSIONS = [".jpg", ".jpeg", ".png", ".bmp", ".webp"]
 
 
-def is_img(img):
-    _, ext = os.path.splitext(img)
-    if ext in imgExt:
+def _is_img(img: Path) -> bool:
+    if img.suffix in IMAGE_EXTENSIONS:
         return True
-    else:
-        return False
+    return False
 
 
-def parse_dir(imgDir):
-    fullpath = os.path.abspath(imgDir)
-    items = os.listdir(fullpath)
-    imgs = [os.path.join(fullpath, name) for name in items if is_img(os.path.join(fullpath, name))]
+def parse_dir(img_dir: Path) -> None:
+    """Parse image directory."""
+    fullpath = Path(img_dir).resolve()
+    items = fullpath.glob("**/*")
+    imgs = [file for file in items if _is_img(file)]
 
     if len(imgs) > 0:
-        comicFileName = os.path.join(imgDir, 'comic.html')
-        sitefile = open(comicFileName, "w+")
-        print('<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>Comic</title></head><body>',
-              file=sitefile)
-        print("<h1>{}</h1>".format(os.path.basename(imgDir)), file=sitefile)
-        for name in imgs:
-            print('<img style="width: 100%; height: auto" src="{}"/>'.format(name.replace("\\\\", "/").
-                                                                                replace("\\", "/")), file=sitefile)
+        comic_strip_file_name = fullpath/"comic.html"
+        with open(comic_strip_file_name, "w+") as sitefile:
+            print('<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>Comic</title></head><body>',
+                file=sitefile)
+            print(f"<h1>{fullpath.stem}</h1>", file=sitefile)
+            for name in imgs:
+                print(f'<img style="width: 100%; height: auto" src="file://{name}"/>', file=sitefile)
+            print("</body></html>", file=sitefile)
 
-        print('</body></html>', file=sitefile)
-        sitefile.close()
-        os.startfile(comicFileName, "open")
+        webbrowser.open(str(comic_strip_file_name))
 
-    for name in items:
-        fullname = os.path.join(fullpath, name)
-        if os.path.isdir(fullname):
+    for fullname in items:
+        if fullname.is_dir():
             parse_dir(fullname)
 
 
 
-def main():
-    handler = argparse.ArgumentParser()
-    handler.add_argument('dir')
-
-    flags = handler.parse_args()
+def _main() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("dir")
+    flags = parser.parse_args()
 
     parse_dir(flags.dir)
 
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    _main()
